79d3ea2eb53b573efdfcc897168856e2be1c175f
sobhanhadikhah <hadikhahs@gmail.com>
2023-03-05 23:34:14 +0330

    added info page

--- src/components/hadeMade.jsx
+++ src/components/hadeMade.jsx
@@ -12,17 +12,17 @@ function HeaderMaderCarder({ img, title, price, id, porcutdss }) {
     return (
         <div className=' border-blue-200 rounded-lg font-SFPRODISPLAYREGULAR ' >
 
-            <Link to={`/products/${id}`} className='flex flex-col    rounded-md border-t-2   mp-2 w-[300px] h-[400px] text-white'  >
+            <Link to={`/products/${id}`} className='flex flex-col    rounded-md border-t-2   mp-2 w-[300px] h-[400px] '  >
                 <div className='bg-white items-center justify-center text-center flex ' >
                     <LazyLoadImage src={img} effect='blur' className='h-[230px] px-3 pt-2 ' alt={title} />
                 </div>
-                <div className='flex flex-col mx-3   ' >
-                    <h3 className='text-black mt-8 ' >{title.length > 60 ? title.slice(0, 60) + "..." : title}</h3>
+                <div className='flex flex-col mx-3 relative  ' >
+                    <h3 className='text-black mt-8 group-hover:underline group-hover:underline-offset-4  ' >{title.length > 60 ? title.slice(0, 60) + "..." : title}</h3>
                 </div>
             </Link>
             <div className='' >
                 <div className='ml-3' >
-                    <p className='font-bold text-black  ' >${price}</p>
+                    <p className='tracking-wide text-gray-900  ' > <span className='text-xs' >$</span> {price}</p>
 
                 </div>
                 <button onClick={handeleAddItem} className='bg-blue-500 max-w-[140px] rounded-md m-2 text-white hover:bg-blue-600 capitalize  py-1 px-[5.5px] relative    ' >add to card</button>
--- src/pages/productsinfo.jsx
+++ src/pages/productsinfo.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import Loading from './loading';
+import useTitle from '../hooks/useTitle';
 function ProductsInfo() {
+    const [Info, setInfo] = useState([])
+    useTitle(Info && Info.title);
     const { id } = useParams()
+    const options = {
+        method: 'GET',
+        url: `https://fakestoreapi.com/products/${id}`,
+    }
+    const fetchProduct = async () => {
+        const mRespone = await axios.request(options).then(function (respone) {
+            return setInfo(respone.data)
+        })
+    }
+    const { isLoading, isSuccess, isError, data, error } = useQuery(
+        [`productsInfo`],
+        fetchProduct
+    )
+    if (isLoading) {
+        return <Loading />
+    }
+
     return (
         <div>
-            {id}
+            {Info && Info.id}
         </div>
     )
 }
